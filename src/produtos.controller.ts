5e0617bbdf3b927c35c5e905d89109b51c9b38d0
Michel Marinho <michel.marinho04@outlook.com>
2022-08-16 08:17:08 -0400

    Add: Models

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -7,33 +7,39 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
+  produtos: Produto[] = [
+    new Produto('LIV01', 'Livro TDD e BDD na prática', 29.9),
+    new Produto('LIV02', 'Livro Iniciando com Flutter', 39.9),
+    new Produto('LIV03', 'Inteligência Artificial como Serviço', 29.9),
+  ];
+
   @Get()
-  obterTodos(): string {
-    return 'Lista todos os produtos';
+  obterTodos(): Produto[] {
+    return this.produtos;
   }
 
   @Get(':id')
-  obterUm(@Param() params): string {
-    return `Retorna os dados do produtos ${params.id}`;
+  obterUm(@Param() params): Produto {
+    return this.produtos[0];
   }
 
   @Post()
-  criar(@Body() produto): string {
-    console.log(produto);
-    return 'Produto Criado';
+  criar(@Body() produto: Produto) {
+    produto.id = 100;
+    this.produtos.push(produto);
   }
 
   @Put()
-  alterar(@Body() produto): string {
-    console.log(produto);
-    return 'Produto Atualizado';
+  alterar(@Body() produto: Produto): Produto {
+    return produto;
   }
 
   @Delete(':id')
-  apagar(@Param() params): string {
-    return `Apaga o produto  ${params.id}`;
+  apagar(@Param() params) {
+    this.produtos.pop();
   }
 }
